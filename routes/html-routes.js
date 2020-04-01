8eb01a54ed104de95d7b157fba7b0efe75b423cf
Your Brennan Predmore <brennnann@gmail.com>
2020-03-31 20:46:57 -0500

    routes connecting main.html to checkout.html init

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -23,7 +23,7 @@ module.exports = function(app) {
   });
 
 
-  app.get("/login", function(req, res) {
+  app.get("/checkout", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
